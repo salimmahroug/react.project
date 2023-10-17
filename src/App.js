9ab852144647b671b6c19b204466535e55663689
salimmahroug <mahrougsalim8@gmail.com>
2023-10-17 15:41:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navbare from './Navv';
+import Carte from './Carte';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my project react</h1>
+     <Navbare/>
+     <Carte/><Carte/><Carte/>
+    </div>
+  );
+}
+
+export default App;
